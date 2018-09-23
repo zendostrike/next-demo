97219b72478655ae380b603b444abb64e7fec206
jinmylopez <jinmy.osiris@gmail.com>
2018-09-23 17:23:55 -0500

    #comment The diagonal styles were added to homepage sections

--- components/styles.js
+++ components/styles.js
@@ -18,8 +18,29 @@ export const SliderContainer = styled.div`
 
 export const Section = styled.div`
   text-align: center;
-  color: ${props => (props.dark ? '#ff' : '#00ca82')};
+  color: ${props => (props.dark ? '#fff' : '#00ca82')};
   background: ${props => props.background || '#fff'};
+  position: relative;
+  padding: ${props => (props.dark ? '1em 1em 9em' : '1em 1em 50px')};
+  margin-top: ${props => (props.dark ? '100px' : '0px')};
+  ::before {
+    content: ' ';
+    background: ${props => (props.dark ? 'linear-gradient(to left bottom, transparent 49.5%, #00b0ea 50%);' : 'linear-gradient(to left bottom, transparent 49.5%, #fff 50%);')};
+    position: absolute;
+    width: 100%;
+    height: 100px;
+    top: ${props => (props.dark ? '-99px' : '-99px')};
+    left: 0;
+  }
+  ::after {
+    content: ' ';
+    background: ${props => (props.dark ? 'linear-gradient(to left bottom, #00b0ea 49.5%, transparent 50%);' : 'linear-gradient(183deg, #fff 49.5%, #F3F3F4 50%);')};
+    position: absolute;
+    width: 100%;
+    height: 100px;
+    bottom: -100px;
+    left: 0;
+  }
 `
 
 export const Select = styled.select`
--- pages/index.js
+++ pages/index.js
@@ -10,15 +10,17 @@ const images = ['/static/images/slide_1.jpg']
 export default () => (
   <Layout>
     <Slider images={images} />
-    <Section>
+    <Section margin-top='100px'>
       Paquetes | Salidas Confirmadas
       <SearchBar />
     </Section>
     <Section background='#00b0ea' dark>
+      Promociones 
+      <SearchBar />
+    </Section>
+    <Section >
       Paquetes | Salidas Confirmadas
       <SearchBar />
     </Section>
-    <Title text={'Mundipack'} />
-    <Subtitle text={'Bienvenidos'} />
   </Layout>
 )
